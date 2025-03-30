3154999f996a0aebe700940bd71d92e9de7b6693
PolinaDemochko <matilda.dp.motion@gmail.com>
2025-03-30 11:02:42 +0300

    init

---
+++ math_result.py
@@ -0,0 +1,8 @@
+import math 
+
+x = int(input("Задайте значення для x: "))
+y = int(input("Задайте значення для y: "))
+i = int(input("Задайте значення для i: "))
+
+result = (((6 + math.tan(x**math.sin(y**math.tan(i**math.cos(y)))) + math.cos(math.sin(math.cos(math.tan(7**x**y)))))/(5 + y - x**math.tan(x*y) + 8 - math.tan(math.cos(math.sin(math.cos(i**math.sin(math.cos(12)))))))) + ((7 + y)/(math.tan(math.cos(math.sin(12))))))
+print(result)
