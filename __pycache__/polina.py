d97f84b9aeb1de62fdcafada5c382ae4a7056be4
PolinaDemochko <matilda.dp.motion@gmail.com>
2025-03-30 12:51:08 +0300

    init

---
+++ __pycache__/polina.py
@@ -0,0 +1,8 @@
+import math 
+
+x = int(input("Задайте значення для x: "))
+y = int(input("Задайте значення для y: "))
+i = int(input("Задайте значення для i: "))
+
+result = ((3 + 8 - (math.cos(4**15)(math.sin(2)(math.tan(7**8) + 4**9043*18*i - 10*math.pi*i**math.pi)) - 3218*2**math.pi**y**12)*31 - 10)/(math.pi**i**x**math.cos(90))*math.sin(3)*math.tan(57) + 32*74*i*y*math.pi*math.cos(4**math.sin(183)))
+print(result)
